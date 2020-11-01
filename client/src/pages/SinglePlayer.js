846fe95a09729cafb410cead3ac9ba6cf01f69f6
aleksandarbojovic8 <aleksandarbojovic8@gmail.com>
2020-11-01 22:20:14 +0100

    single player page

--- client/src/pages/SinglePlayer.js
+++ client/src/pages/SinglePlayer.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import styles from './SinglePlayer.module.css';
+import JambTable from '../components/Table/JambTable';
 
 export default function SinglePlayer() {
-  return <div>Hello World Single Player Page</div>;
+  return (
+    <div className={styles.container}>
+      <div>DICES</div>
+      <JambTable />
+    </div>
+  );
 }
