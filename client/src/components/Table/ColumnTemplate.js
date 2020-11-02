d53059ceac02f524d173761ff3b2c110403c9129
aleksandarbojovic8 <aleksandarbojovic8@gmail.com>
2020-11-02 03:35:46 +0100

    column template first version

---
+++ client/src/components/Table/ColumnTemplate.js
@@ -0,0 +1,187 @@
+import React from 'react';
+import styles from './ColumnTemplate.module.css';
+
+export default function ColumnTemplate(props) {
+  const { name } = props;
+
+  const obj = {
+    one: null,
+    two: null,
+    three: null,
+    four: null,
+    five: null,
+    six: null,
+    upperSum: null,
+    max: null,
+    min: null,
+    middleSum: null,
+    kenta: null,
+    triling: null,
+    ful: null,
+    poker: null,
+    jamb: null,
+    bottomSum: null
+  };
+
+  let rollNumber = 1;
+
+  //   topToBottom: {},
+  //   fromTopAndBottom: {},
+  //   bottomToTop: {},
+  //   annunciation: {},
+  //   fromHand: {},
+  //   alert: {},
+  //   fromMiddle: {},
+  //   toMiddle: {},
+  //   lastCol: {},
+  //   maxCol: {}
+
+  const fillField = e => {
+    const id = e.target.id;
+    if (id === 'upperSum' || id === 'middleSum' || id === 'bottomSum') {
+      return;
+    }
+    //call action to fill data on this field
+    console.log(id);
+  };
+
+  const keyArray = Object.keys(obj);
+
+  const enabledFields = [];
+
+  if (name === 'topToBottom') {
+    let foundOne = false;
+    keyArray.forEach(val => {
+      if (
+        foundOne === false &&
+        obj[val] === null &&
+        val !== 'upperSum' &&
+        val !== 'middleSum' &&
+        val !== 'bottomSum'
+      ) {
+        enabledFields.push(val);
+        foundOne = true;
+      }
+    });
+  }
+
+  if (name === 'fromTopAndBottom') {
+    keyArray.forEach(val => {
+      if (val !== 'upperSum' && val !== 'middleSum' && val !== 'bottomSum') {
+        enabledFields.push(val);
+      }
+    });
+  }
+
+  if (name === 'bottomToTop') {
+    let foundOne = false;
+    keyArray
+      .slice()
+      .reverse()
+      .forEach(val => {
+        if (
+          foundOne === false &&
+          obj[val] === null &&
+          val !== 'upperSum' &&
+          val !== 'middleSum' &&
+          val !== 'bottomSum'
+        ) {
+          enabledFields.push(val);
+          foundOne = true;
+        }
+      });
+  }
+
+  if (name === 'fromHand') {
+    keyArray.forEach(val => {
+      if (
+        val !== 'upperSum' &&
+        val !== 'middleSum' &&
+        val !== 'bottomSum' &&
+        rollNumber === 1
+      ) {
+        enabledFields.push(val);
+      }
+    });
+  }
+
+  if (name === 'fromMiddle') {
+    let foundOne = false;
+    let foundTwo = false;
+    keyArray
+      .slice(0, 8)
+      .reverse()
+      .forEach(val => {
+        if (foundOne === false && obj[val] === null && val !== 'upperSum') {
+          enabledFields.push(val);
+          foundOne = true;
+        }
+      });
+
+    keyArray.slice(8).forEach(val => {
+      if (
+        foundTwo === false &&
+        obj[val] === null &&
+        val !== 'middleSum' &&
+        val !== 'bottomSum'
+      ) {
+        enabledFields.push(val);
+        foundTwo = true;
+      }
+    });
+  }
+
+  if (name === 'toMiddle') {
+    let foundOne = false;
+    let foundTwo = false;
+    keyArray.slice(0, 8).forEach(val => {
+      if (foundOne === false && obj[val] === null && val !== 'upperSum') {
+        enabledFields.push(val);
+        foundOne = true;
+      }
+    });
+
+    keyArray
+      .slice(8)
+      .reverse()
+      .forEach(val => {
+        if (
+          foundTwo === false &&
+          obj[val] === null &&
+          val !== 'middleSum' &&
+          val !== 'bottomSum'
+        ) {
+          enabledFields.push(val);
+          foundTwo = true;
+        }
+      });
+  }
+
+  if (name === 'maxCol') {
+    keyArray.forEach(val => {
+      if (val !== 'upperSum' && val !== 'middleSum' && val !== 'bottomSum') {
+        enabledFields.push(val);
+      }
+    });
+  }
+
+  return (
+    <>
+      {keyArray.map(val => {
+        let enabled = enabledFields.includes(val);
+        return (
+          <div
+            id={val}
+            key={val}
+            onClick={fillField}
+            className={`${styles.field} ${
+              enabled ? styles.nextField : styles.fieldDisabled
+            }`}
+          >
+            {obj[val]}
+          </div>
+        );
+      })}
+    </>
+  );
+}
--- client/src/components/Table/JambTable.js
+++ client/src/components/Table/JambTable.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './JambTable.module.css';
 
 import NamesColumn from './NamesColumn';
+import ColumnTemplate from './ColumnTemplate';
 
 export default function JambTable() {
   return (
@@ -9,16 +10,43 @@ export default function JambTable() {
       <div className={styles.column}>
         <NamesColumn />
       </div>
-      <div className={styles.column}>&#8595;</div>
-      <div className={styles.column}>&#8645;</div>
-      <div className={styles.column}>&#x2191;</div>
-      <div className={styles.column}>N</div>
-      <div className={styles.column}>R</div>
-      <div className={styles.column}>D</div>
-      <div className={styles.column}>&#x2191;/&#8595;</div>
-      <div className={styles.column}>&#8595;/&#x2191;</div>
-      <div className={styles.column}>O</div>
-      <div className={styles.column}>M</div>
+      <div className={styles.column}>
+        <span>&#8595;</span>
+        <ColumnTemplate name="topToBottom" />
+      </div>
+      <div className={styles.column}>
+        <span>&#8645;</span>
+        <ColumnTemplate name="fromTopAndBottom" />
+      </div>
+      <div className={styles.column}>
+        <span>&#x2191;</span>
+        <ColumnTemplate name="bottomToTop" />
+      </div>
+      {/* <div className={styles.column}>
+        <span>N</span>
+      </div> */}
+      <div className={styles.column}>
+        <span>R</span>
+        <ColumnTemplate name="fromHand" />
+      </div>
+      {/* <div className={styles.column}>
+        <span>D</span>
+      </div> */}
+      <div className={styles.column}>
+        <span>&#x2191;/&#8595;</span>
+        <ColumnTemplate name="fromMiddle" />
+      </div>
+      <div className={styles.column}>
+        <span>&#8595;/&#x2191;</span>
+        <ColumnTemplate name="toMiddle" />
+      </div>
+      {/* <div className={styles.column}>
+        <span>O</span>
+      </div> */}
+      <div className={styles.column}>
+        <span>M</span>
+        <ColumnTemplate name="maxCol" />
+      </div>
       <div className={styles.column}>/</div>
     </div>
   );
